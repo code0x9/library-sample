cd7731eb3ad267b20d176315ed93717968209008
mark <mark@titicaca-corp.com>
2018-02-23 18:34:25 +0900

    bugfix: external file on classpath is not visible in jar file.

--- src/main/kotlin/com/mark/library/service/MailService.kt
+++ src/main/kotlin/com/mark/library/service/MailService.kt
@@ -3,10 +3,8 @@ package com.mark.library.service
 import com.mark.library.domain.LoanRepository
 import com.mark.library.ext.toDate
 import org.rythmengine.Rythm
-import org.springframework.core.io.ClassPathResource
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
-import java.io.File
 
 data class Mail(
         val from: String,
@@ -15,12 +13,16 @@ data class Mail(
         val text: String
 )
 
+const val RECALL_TEMPLATE = """
+@args Date dueDate
+<중앙2문헌실>
+귀하(혹은 가족)의 도서반납일은 @dueDate.format("M월 d일(E)", Locale.KOREA)입니다.
+"""
+
 @Service
 class MailService(
         private val loanRepo: LoanRepository
 ) {
-    val recallTemplate: File = ClassPathResource("templates/recall.txt").file
-
     @Scheduled(cron = "0 0 10 * * *")
     fun sendmail() {
         val loans = loanRepo.findByDueDateEquals()
@@ -29,7 +31,7 @@ class MailService(
                     from = "loan@library.gov",
                     to = it.email,
                     subject = "도서 반납 안내",
-                    text = Rythm.render(recallTemplate, it.dueDate.toDate()))
+                    text = Rythm.render(RECALL_TEMPLATE, it.dueDate.toDate()))
         }
         println(mails)
     }
